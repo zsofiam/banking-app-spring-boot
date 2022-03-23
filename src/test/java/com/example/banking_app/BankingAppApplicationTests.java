96db85243f6836b2ebecd650066fa0bbe02a3a61
Zsofia Matyasi <zsofia.matyasi@gmail.com>
2022-03-23 14:59:45 +0100

    [test] context loads

--- src/test/java/com/example/banking_app/BankingAppApplicationTests.java
+++ src/test/java/com/example/banking_app/BankingAppApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.banking_app;
 
+import com.example.banking_app.controller.AccountController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BankingAppApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
+	}*/
+
+	@Autowired
+	private AccountController controller;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
